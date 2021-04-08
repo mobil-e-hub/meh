7f299f57164444dbacd9b8e77819ff77601fc757
Michael Pernpeintner <pernpeintner@es.uni-mannheim.de>
2021-04-08 09:05:55 +0200

    Update endpoints of simulator

--- src/simulator/app.js
+++ src/simulator/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const _ = require('lodash');
+const morgan = require('morgan');
 
 // Internal modules
 const DroneSimulator = require('./simulators/drone-simulator');
@@ -20,6 +21,7 @@ const app = express();
 const port = 3000;
 app.use(cors());
 app.use(bodyParser.json());
+app.use(morgan('combined'));
 
 const server = app.listen(port, () => {
     console.log(`< Server listening at http://localhost:${port}.`)
@@ -53,26 +55,30 @@ function shutdown() {
 
 // Endpoints
 app.get('/ping', (req, res) => {
-    res.json('pong');
+    res.status(200).json('pong');
 });
 
-app.post('/meh/viz/hubs/find', (req, res) => {
-    res.json(controlSystem.getHubs(req.body.position, req.body.radius));
+app.post('/eventgrid', (req, res) => {
+    res.status(200).end();
 });
 
-app.post('/meh/viz/orders/request', (req, res) => {
-    res.json({
-        sourceHubs: controlSystem.getHubs(req.body.source.position, req.body.source.radius).map(h => ({ id: h.id, position: topology.nodes[h.position].position })),
-        destinationHubs: controlSystem.getHubs(req.body.destination.position, req.body.destination.radius).map(h => ({ id: h.id, position: topology.nodes[h.position].position }))
-    });
-});
-
-app.post('/meh/viz/orders/place', (req, res) => {
-    const sourceHub = hubSimulator.hubs[req.body.destinationHubs];
-    const destinationHubs = _.pick(hubSimulator.hubs, req.body.destinationHubs);
-
-    parcelSimulator.
-    controlSystem.
-
-    res.status(200).json('ok');
-});
+// app.post('/meh/viz/hubs/find', (req, res) => {
+//     res.json(controlSystem.getHubs(req.body.position, req.body.radius));
+// });
+//
+// app.post('/meh/viz/orders/request', (req, res) => {
+//     res.json({
+//         sourceHubs: controlSystem.getHubs(req.body.source.position, req.body.source.radius).map(h => ({ id: h.id, position: topology.nodes[h.position].position })),
+//         destinationHubs: controlSystem.getHubs(req.body.destination.position, req.body.destination.radius).map(h => ({ id: h.id, position: topology.nodes[h.position].position }))
+//     });
+// });
+//
+// app.post('/meh/viz/orders/place', (req, res) => {
+//     const sourceHub = hubSimulator.hubs[req.body.destinationHubs];
+//     const destinationHubs = _.pick(hubSimulator.hubs, req.body.destinationHubs);
+//
+//     parcelSimulator.
+//     controlSystem.
+//
+//     res.status(200).json('ok');
+// });
