2add52cccaf42beaa03a3a59c71361da5553b5a9
Alex <albecker@mail.uni-mannheim.de>
2022-01-25 14:05:15 +0100

    save progress on ParcelState

--- sim/src/models/parcel.js
+++ sim/src/models/parcel.js
@@ -1,7 +1,19 @@
 // External modules
 
+
+const ParcelState = {
+    transportCreated: 0,
+    waitingForTransport: 1,
+    inTransport_InAir: 2,
+    inTransport:3,
+    delivered:4,
+    completed:5,
+    cancelled:6,
+    failed:7
+}
+
 module.exports = class Parcel {
-    constructor(id, carrier, destination) {
+    constructor(id, carrier, destination, state=ParcelState.transportCreated) {
         this.id = id;
         this.carrier = carrier;
         this.destination = destination;
