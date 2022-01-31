40b032f5693af70af5079c97a650a8cf0213201d
mobil-e-hub <pernpeintner@es.uni-mannheim.de>
2022-01-31 19:44:37 +0100

    Update connector to new Orchestrator format

---
+++ connector/src/schemas/orderPlacedSchema.js
@@ -0,0 +1,27 @@
+module.exports = {
+	"type": "object",
+	"properties": {
+    "boxId": { "type": "string", "format": "uuid" },
+		"transportId": { "type": "string", "format": "uuid" },
+    "partnerId": { "type": "string", "format": "uuid" },
+    "timestamp": { "type": "string", "format": "date-time" },
+    "startLocation": {
+      "type": "object",
+      "properties": {
+        "platformId": { "type": "string", "format": "uuid" }
+      },
+      "required": ["platformId"],
+      "additionalProperties": false
+    },
+    "destinationLocation": {
+      "type": "object",
+      "properties": {
+        "platformId": { "type": "string", "format": "uuid" }
+      },
+      "required": ["platformId"],
+      "additionalProperties": false
+    }
+	},
+	"required": ["boxId", "transportId","partnerId", "timestamp", "startLocation", "destinationLocation"],
+  "additionalProperties": false
+};
--- connector/src/server.js
+++ connector/src/server.js
@@ -1,21 +1,23 @@
 // External modules
 const express = require('express');
 const cors = require('cors');
-const bodyParser = require('body-parser');
 const _ = require('lodash');
 const morgan = require('morgan');
 const mqttMatch = require('mqtt-match');
-const { EventGridPublisherClient, AzureKeyCredential } = require("@azure/eventgrid");
 const dotenv = require('dotenv');
 const MQTT = require('mqtt');
 const { Validator, ValidationError } = require('express-json-validator-middleware');
-
+const axios = require('axios');
 
 // Environment variables
 dotenv.config();
 
-const eventGridEndpoint = process.env.EVENT_GRID_ENDPOINT;
-const eventGridKey = process.env.EVENT_GRID_KEY;
+// Orchestrator config
+const orchestrator = {
+    url: process.env.ORCHESTRATOR_URL,
+    subscriptionKey: process.env.ORCHESTRATOR_SUBSCRIPTION_KEY
+}
+
 const port = process.env.SERVER_PORT || 3004;
 
 const brokerUrl = process.env.MQTT_BROKER_URL;
@@ -27,11 +29,11 @@ const version = process.env.VERSION || 'v1';
 
 // JSON schema validation
 const { validate } = new Validator();
-const gridSchema = require("./schema")
+const orderPlacedSchema = require("./schemas/orderPlacedSchema");
 
-// Topics to listen to from MQTT broker (make sure that they are disjoint from topics received from EventGrid!)
+// Topics to listen to from MQTT broker (make sure that they are disjoint from topics received from Orchestrator!)
 const subscriptionTopics = [
-    'drone/#'
+    'parcel/+/status'
 ];
 
 function validationErrorMiddleware(error, request, response, next) {
@@ -69,14 +71,11 @@ app.use((error, req, res, next) => {
     }
 });
 
-
-
 const server = app.listen(port, () => {
     console.log(`< (connector) Server listening at http://localhost:${port}.`);
 });
 
-
-// MQTT client
+// Start MQTT client
 const mqttClient = MQTT.connect(brokerUrl, {
                 username: brokerUsername,
                 password: brokerPassword
@@ -90,108 +89,70 @@ mqttClient.on('connect', () => {
 });
 
 
-// EventGrid client (@azure/eventgrid)
-const eventGridClient = new EventGridPublisherClient(eventGridEndpoint, 'EventGrid', new AzureKeyCredential(eventGridKey));
-
-
 // Endpoints
 // Base
 app.get('/', (req, res) => {
     res.status(200).json('This is the base url of the meh/connector module.');
 });
 
-
 //Ping
 app.get('/ping', (req, res) => {
     res.status(200).json({ connector: 'pong' });
 });
-
 
 // TODO: - move async to function?
 //      - where exactly to place next() calls?
 //      - split into seperate files -> Error handling ()
 //       - move schema out of src folder
 
-// Receive events from EventGrid and forward them to MQTT broker
-app.post('/', validate({body: gridSchema}), async (req, res, next) => {
+// Receive events from Orchestrator and forward them to MQTT broker
+// The only valid event received from Orchestrator is an order/placed event -> validate immediately
+app.post('/', validate({body: orderPlacedSchema}), async (req, res, next) => {
     try {
+        const message = req.body;
+        const topic = `${root}/${version}/order/${message.transportId}/placed`;
 
-        // EventGrid messages are sent as an array
-        if (Array.isArray(req.body)) {
-            for (const event of req.body) {
-                // If this is a validation request, reply appropriately
-                if (event.eventType === "Microsoft.EventGrid.SubscriptionValidationEvent") {
-                    try {
-                        console.log(`> (connector) SubscriptionValidation received from EventGrid: Validation code = ${event.data.validationCode}, topic = ${event.topic}`);
-                        res.status(200).json({ValidationResponse: event.data.validationCode});
-                    } catch (err) {
-                        res.status(400).end();
-                    }
-                    next();
-                }
-                // If it is a simple echo, log it
-                else if (event.eventType === "Portal_Echo") {
-                    console.log(`> (connector) Echo received from EventGrid!`);
-                    next();
-                }
-                // If it has the correct root and version, process it
-                else if (event.eventType === root && event.dataVersion === version) {
-                    try {
-                        const topic = event.subject;
-                        const message = JSON.stringify(event.data);
-                        let [entity, id, ...args] = topic.split('/');
+        // mqttClient.publish(topic, JSON.stringify(message));
     
-                        // Forward message to MQTT broker
-                        mqttClient.publish(`${root}/${version}/${topic}`, message);
-    
-                        console.log(`  (connector) Forwarding ${topic}: ${message} from EventGrid to MQTT`);
-                    } catch (err) {
-                        console.log(`> (connector) Invalid event received from EventGrid: ${JSON.stringify({ event, err })}`);
-                    }
-                    next();
-                }
-                // Otherwise, log it
-                else {
-                    console.log(`> (connector) Invalid message received: ${JSON.stringify(req.body)}`);
-                    return res.status(400).end();
-                }
-            }
-            // All messages forwarded
-            return res.status(200).end();
-        }
-        else {
-            console.log(`> (connector) Invalid message received: ${JSON.stringify(req.body)}`);
-            return res.status(400).end();
-        }
+        console.log(`  (connector) Forwarding ${topic}: ${message} from Orchestrator to MQTT`);
+
+        // There's no need to return anything except status 200. Return json just for human readability (e.g., from Postman))
+        res.status(200).json({ success: true, message: `Message forwarded to MQTT with topic ${topic}.` });
     } 
     catch (error) {
         //TODO replace with expressjs middleware error Handling  --> https://www.robinwieruch.de/node-express-error-handling/
         console.log(`> (connector) Invalid message received: ${JSON.stringify({ body: req.body, error })}`);
         return res.status(400).end();
     }
-
-    // There's no need to return anything except status 200
-    res.status(200).end();
 });
 
-// Receive messages from MQTT broker and forward them to EventGrid
+// Receive messages from MQTT broker and forward them to Orchestrator
 mqttClient.on('message', async (topic, message) => {
     try {
         let [root, version, ...args] = topic.split('/');
         topic = args.join('/');
         message = JSON.parse(message.toString());
 
-        // Forward message to EventGrid
-        try {
-            await eventGridClient.send([{ eventType: root, dataVersion: version, subject: topic, data: message }]);
-        }
-        catch (err) {
-            console.log(`< (connector) Could not forward ${topic}: ${JSON.stringify(message)} to EventGrid: ${err}`);
+        // TODO: Validate message format against schema
+        if (false) {
+            console.log(`> (connector) Invalid event received from MQTT broker: ${message}`);
         }
 
-        console.log(`  (connector) Forwarding ${topic}: ${JSON.stringify(message)} from MQTT to EventGrid`);
+        // Forward message to Orchestrator
+        headers = { 
+            'Ocp-Apim-Subscription-Key': orchestrator.subscriptionKey 
+        };
+        
+        body = {
+            transportId: message.id,
+            address: { platformId: message.carrier },
+            state: message.state
+        }
+
+        result = await axios.post(orchestrator.url, body, { headers });
+        console.log(`  (connector) Forwarding ${topic}: ${message} from MQTT to Orchestrator.`);
     }
     catch (err) {
-        console.log(`> (connector) Invalid event received from MQTT broker: ${err}`);
+        console.log(`  (connector) Could not forward ${topic}: ${message} from MQTT to Orchestrator!`);
     }
 });
