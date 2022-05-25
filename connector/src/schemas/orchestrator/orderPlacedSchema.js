01c2ff0d1e383c039734e82698ed22d42cd0a56a
Alex <albecker@mail.uni-mannheim.de>
2022-05-25 18:17:54 +0200

    replace uuid format with 18 digit number check in JSON Schema for incoming OrderPlaced Messages

--- connector/src/schemas/orchestrator/orderPlacedSchema.js
+++ connector/src/schemas/orchestrator/orderPlacedSchema.js
@@ -1,8 +1,12 @@
 module.exports = {
+    "$id": "https://ines-gpu-01.informatik.uni-mannheim.de/meh/schemas/orchestrator/OrderPlaced.json",
+    "$schema": "https://json-schema.org/draft/2020-12/schema",
+    "title": "OrderPlaced",
+    "description": "JSON Schema for order placed messages. ",
 	"type": "object",
 	"properties": {
     "boxId": { "type": "string", "format": "uuid" },
-	"transportId": { "type": "string", "format": "uuid" },
+	"transportId": { "type": "string", "pattern": "^\\d{18}$"},
     "partnerId": { "type": "string", "format": "uuid" },
     "timestamp": { "type": "string", "format": "date-time" },
     "startLocation": {
