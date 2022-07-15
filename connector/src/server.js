663c521b541bb905ece42087e120116ed9b2035d
Michael Pernpeintner <pernpeintner@es.uni-mannheim.de>
2022-07-15 11:28:39 +0200

    feature: Allow connector to handle multiple topics in both directions.

--- connector/src/server.js
+++ connector/src/server.js
@@ -40,48 +40,83 @@ const schemas = {
     }
 }
 
-// Topics to listen to from MQTT broker (make sure that they are disjoint from topics received from Orchestrator!)
-const subscriptionTopics = [
-    'parcel/+/transfer',
-    'parcel/+/delivered',
-    'parcel/+/collected',
-    'parcel/+/removed'
-];
-
-function validationErrorMiddleware(error, request, response, next) {
-	 if (error instanceof ValidationError) {
-    // Handle the error
-    response.status(400).send(error.validationErrors);
-    next();
-  } else {
-    // Pass error on if not a validation error
-    next(error);
-  }
+const forwardings = {
+    mqttToHttp: {
+        // Topics to listen to from MQTT broker (make sure that they are disjoint from topics received from Orchestrator!)
+        'parcel/+/transfer': {
+            inputSchema: schemas.mqtt.parcelSchema,
+            outputSchema: schemas.orchestrator.statusUpdateSchema,
+            handler: (topic, message) => {
+                return {
+                    boxId: message.id,
+                    transportId: message.orderId,
+                    location: {platformId: message.carrier.id},
+                    state: message.carrier.type === 'drone' ? 'InTransportInAir' : (message.carrier.type === 'car' ? 'InTransport' : 'WaitingForTransport')
+                };
+            }
+        },
+        'parcel/+/delivered': {
+            inputSchema: schemas.mqtt.parcelSchema,
+            outputSchema: schemas.orchestrator.statusUpdateSchema,
+            handler: (topic, message) => {
+                return { boxId: message.id, transportId: message.orderId, location: {platformId: message.carrier.id}, state: 'Delivered' };
+            }
+        },
+        'parcel/+/collected': {
+            inputSchema: schemas.mqtt.parcelSchema,
+            outputSchema: schemas.orchestrator.statusUpdateSchema,
+            handler: (topic, message) => {
+                return { boxId: message.id, transportId: message.orderId, location: {platformId: message.carrier.id}, state: 'Completed' };
+            }
+        },
+        'parcel/+/removed': {
+            inputSchema: schemas.mqtt.parcelSchema,
+            outputSchema: schemas.orchestrator.statusUpdateSchema,
+            handler: (topic, message) => {
+                return { boxId: message.id, transportId: message.orderId, location: {platformId: message.carrier.id}, state: 'Failed' };
+            }
+        }
+    },
+    httpToMqtt: {
+        'orderPlaced': {
+            inputSchema: schemas.orchestrator.orderPlacedSchema,
+            outputSchema: schemas.mqtt.parcelSchema,
+            handler: (req, res) => {
+                return {
+                    topic: `${root}/${version}/order/${req.body.transportId}/placed`,
+                    message: {
+                        id: req.body.boxId,
+                        orderId: req.body.transportId,
+                        carrier: null,
+                        destination: {type: 'hub', id: req.body.destinationLocation.platformId}
+                    }
+                };
+            }
+        }
+    }
 }
-
 
 // Server
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-// app.use(validationErrorMiddleware);
 /**
  * Error handler middleware for validation errors.
  */
-app.use((error, req, res, next) => {
-    // Check the error is a validation error
-    console.log(`< (connector) Error caught: ${error}.`);
-    if (error instanceof ValidationError) {
-        // Handle the error
-        console.log(`< (connector) Validation Error against JSON schema caught: ${error}.`);
-        res.status(400).send(error.validationErrors);
-        next();
-    } else {
-        // Pass error on if not a validation error
-        next(error);
-    }
-});
+// app.use((error, req, res, next) => {
+//     // Check the error is a validation error
+//     console.log(`< (connector) Error caught: ${error}.`);
+//     if (error instanceof ValidationError) {
+//         // Handle the error
+//         console.log(`< (connector) Validation Error against JSON schema caught: ${error}.`);
+//         res.status(400).send(error.validationErrors);
+//         next();
+//     } else {
+//         // Pass error on if not a validation error
+//         next(error);
+//     }
+// });
 
 const server = app.listen(port, () => {
     console.log(`< (connector) Server listening at http://localhost:${port}.`);
@@ -96,8 +131,8 @@ const mqttClient = MQTT.connect(brokerUrl, {
 // Listen to any message which starts with the correct root and version
 mqttClient.on('connect', () => {
     console.log(`< (connector) MQTT client connected to broker at ${brokerUrl}.`);
-    mqttClient.subscribe(subscriptionTopics.map(topic => `${root}/${version}/${topic}`));
-    console.log(`< (connector) MQTT client subscribed to topics ${subscriptionTopics.join(', ')}.`);
+    mqttClient.subscribe(Object.keys(forwardings.mqttToHttp).map(topic => `${root}/${version}/${topic}`));
+    console.log(`< (connector) MQTT client subscribed to topics ${Object.keys(forwardings.mqttToHttp).join(', ')}.`);
 });
 
 
@@ -112,35 +147,33 @@ app.get('/ping', (req, res) => {
     res.status(200).json({ connector: 'pong' });
 });
 
-// TODO: - move async to function?
-//      - where exactly to place next() calls?
-//      - split into seperate files -> Error handling ()
-
 // Receive events from Orchestrator and forward them to MQTT broker
-// The only valid event received from Orchestrator is an order/placed event -> validate immediately
-app.post('/', validate({body: schemas.orchestrator.orderPlacedSchema}), async (req, res, next) => {
-    try {
-        const topic = `${root}/${version}/order/${req.body.transportId}/placed`;
-        const message = {
-            id: req.body.boxId,
-            orderId: req.body.transportId,
-            carrier: null,
-            destination: {
-                type: 'hub',
-                id: req.body.destinationLocation.platformId
+app.post('/', async (req, res, next) => {
+    for (const [key, [inputSchema, outputSchema, handler]] of Object.entries(forwardings.httpToMqtt)) {
+        if (true) { // TODO: Check if handler is appropriate for message
+            try {
+                if (!inputSchema || schemaValidator.validate(req.body, inputSchema).valid) {
+                    const [topic, message] = handler(req, res);
+                    if (!outputSchema || schemaValidator.validate(message, outputSchema).valid) {
+                        mqttClient.publish(topic, JSON.stringify(message));
+                        console.log(`  (connector) Forwarding ${topic}: ${JSON.stringify(message)} from Orchestrator to MQTT.`);
+
+                        // There's no need to return anything except status 200. Return json just for human readability (e.g., from Postman))
+                        res.status(200).json({ success: true, message: `Message forwarded to MQTT with topic ${topic}.` });
+                    }
+                    else {
+                        console.log(`> (connector) Output schema validation failed: ${schemaValidator.validate(message, outputSchema)}`);
+                    }
+                }
+                else {
+                    console.log(`> (connector) Input schema validation failed: ${schemaValidator.validate(req.body, inputSchema)}`);
+                }
+            }
+            catch (e) {
+                console.log(`> (connector) Could not forward ${req.body} from Orchestrator to MQTT: ${e}`);
+                return res.status(400).end();
             }
         }
-
-        mqttClient.publish(topic, JSON.stringify(message));
-
-        console.log(`  (connector) Forwarding ${topic}: ${JSON.stringify(message)} from Orchestrator to MQTT`);
-
-        // There's no need to return anything except status 200. Return json just for human readability (e.g., from Postman))
-        res.status(200).json({ success: true, message: `Message forwarded to MQTT with topic ${topic}.` });
-    }
-    catch (error) {
-        console.log(`> (connector) Invalid message received: ${JSON.stringify({ body: req.body, error })}`);
-        return res.status(400).end();
     }
 });
 
@@ -150,102 +183,31 @@ mqttClient.on('message', async (topic, message) => {
         let [root, version, ...args] = topic.split('/');
         topic = args.join('/');
         message = JSON.parse(message);
-
-        let body = null;
-
-        if (mqttMatch('parcel/+/transfer', topic)) {
-            // Convert into statusUpdate format
-            const validationResult = schemaValidator.validate(message, schemas.mqtt.parcelSchema);
-            if (validationResult.valid) {
-                body = {
-                    boxId: message.id,
-                    transportId: message.orderId,
-                    location: { platformId: message.carrier.id },
-                    state: message.carrier.type === 'drone' ? 'InTransportInAir' : (message.carrier.type === 'car' ? 'InTransport' : 'WaitingForTransport')
-                }
-                if (!schemaValidator.validate(body, schemas.orchestrator.statusUpdateSchema).valid) {
-                    console.log(`> (connector) Could not transform message: ${JSON.stringify(message)}`);
-                    return;
-                }
-            }
-            else {
-                console.log(`> (connector) Invalid event received from MQTT broker: ${message}. Validation result: ${JSON.stringify(validationResult)}`);
-                return;
-            }
-        }
-        else if (mqttMatch('parcel/+/delivered', topic)) {
-            // Convert into statusUpdate format
-            const validationResult = schemaValidator.validate(message, schemas.mqtt.parcelSchema);
-            if (validationResult.valid) {
-                body = {
-                    boxId: message.id,
-                    transportId: message.orderId,
-                    location: { platformId: message.carrier.id },
-                    state: 'Delivered'
-                }
-
-                if (!schemaValidator.validate(body, schemas.orchestrator.statusUpdateSchema).valid) {
-                    console.log(`> (connector) Could not transform message: ${message}`);
-                    return;
-                }
-            }
-            else {
-                console.log(`> (connector) Invalid event received from MQTT broker: ${message}`);
-                return;
-            }
-        }
-        else if (mqttMatch('parcel/+/collected', topic)) {
-            // Convert into statusUpdate format
-            const validationResult = schemaValidator.validate(message, schemas.mqtt.parcelSchema);
-            if (validationResult.valid) {
-                body = {
-                    boxId: message.id,
-                    transportId: message.orderId,
-                    location: { platformId: message.carrier.id },
-                    state: 'Completed'
-                }
-
-                if (!schemaValidator.validate(body, schemas.orchestrator.statusUpdateSchema).valid) {
-                    console.log(`> (connector) Could not transform message: ${JSON.stringify(message)}`);
-                    return;
-                }
-            }
-            else {
-                console.log(`> (connector) Invalid event received from MQTT broker: ${JSON.stringify(message)}`);
-                return;
-            }
-        }
-        else if (mqttMatch('parcel/+/removed', topic)) {
-            // Convert into statusUpdate format
-            const validationResult = schemaValidator.validate(message, schemas.mqtt.parcelSchema);
-            if (validationResult.valid) {
-                body = {
-                    boxId: message.id,
-                    transportId: message.orderId,
-                    location: { platformId: message.carrier.id },
-                    state: 'Failed'
-                }
-
-                if (!schemaValidator.validate(body, schemas.orchestrator.statusUpdateSchema).valid) {
-                    console.log(`> (connector) Could not transform message: ${JSON.stringify(message)}`);
-                    return;
-                }
-            }
-            else {
-                console.log(`> (connector) Invalid event received from MQTT broker: ${JSON.stringify(message)}`);
-                return;
-            }
-        }
-        else if (false) {
-            // TODO: add other messages for forwarding to the Orchestrator
-        }
-
-        // Forward message to Orchestrator
-        const result = await axios.post(orchestrator.url, body, { headers: { 'Ocp-Apim-Subscription-Key': orchestrator.subscriptionKey, 'Content-Type': 'application/json' } });
-        console.log(`  (connector) Forwarding ${topic}: ${JSON.stringify(message)} from MQTT to Orchestrator as ${JSON.stringify(body)}.`);
     }
     catch (err) {
-        // console.log(`  (connector) Could not forward message ${JSON.stringify(body)} from MQTT to Orchestrator: ${err}!`);
-        console.log(`  (connector) Could not forward ${topic}: ${JSON.stringify(message)} from MQTT to Orchestrator: ${err}!`);
+        console.log(`  (connector) Could not parse ${topic}: ${JSON.stringify(message)}: ${err}!`);
+    }
+
+    for (const [key, [inputSchema, outputSchema, handler]] of Object.entries(forwardings.mqttToHttp)) {
+        if (mqttMatch(key, topic)) {
+            try {
+                if (!inputSchema || schemaValidator.validate(message, inputSchema).valid) {
+                    const body = handler(topic, message);
+                    if (!outputSchema || schemaValidator.validate(body, outputSchema).valid) {
+                        await axios.post(orchestrator.url, body, { headers: { 'Ocp-Apim-Subscription-Key': orchestrator.subscriptionKey, 'Content-Type': 'application/json' } });
+                        console.log(`  (connector) Forwarding ${topic}: ${JSON.stringify(message)} from MQTT to Orchestrator as ${JSON.stringify(body)}.`);
+                    }
+                    else {
+                        console.log(`> (connector) Output schema validation failed: ${schemaValidator.validate(body, outputSchema)}`);
+                    }
+                }
+                else {
+                    console.log(`> (connector) Input schema validation failed: ${schemaValidator.validate(message, inputSchema)}`);
+                }
+            }
+            catch (e) {
+                console.log(`> (connector) Could not forward ${topic} (${JSON.stringify(message)}) from MQTT to Orchestrator: ${e}`);
+            }
+        }
     }
 });
