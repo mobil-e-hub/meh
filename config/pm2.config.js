959e6cf83d514de16d967434667274663cb3e194
Michael Pernpeintner <michael.oesterle@uni-mannheim.de>
2023-05-24 22:38:49 +0200

    Configure pm2

---
+++ config/pm2.config.js
@@ -0,0 +1,46 @@
+module.exports = {
+    apps : [
+        {
+            name: "sim",
+            script: "./sim/src/server.js",
+            watch: true,
+            restart_delay: 3000
+
+        },
+        {
+            name: "opt",
+            script: "./opt/src/server.py",
+            watch: true,
+            restart_delay: 3000
+
+        },
+        // {
+        //     name: "viz",
+        //     script: "cd viz && vue-cli-service serve",
+        //     watch: false,
+        //     // restart_delay: 3000
+
+        // },
+        {
+            name: "mqtt",
+            script: "mosquitto",
+            // watch: true,
+            // restart_delay: 3000
+
+        },
+        {
+            name: "monitoring",
+            script: "cd monitoring && ng serve --live-reload=false",
+            watch: true,
+            restart_delay: 3000
+
+        },
+        {
+            name: "git",
+            script: "./git/src/server.js",
+            watch: true,
+            restart_delay: 3000
+
+        }
+    ]
+}
