76f35923c04e29ccc40cfb32669a01291caab379
johannespernpeintner <johannes.pernpeintner@tum.de>
2023-06-08 07:42:02 +0200

    Update optimization_engine.py

--- opt/src/optimization_engine/optimization_engine.py
+++ opt/src/optimization_engine/optimization_engine.py
@@ -97,11 +97,14 @@ class OptimizationEngineShowcase0(OptimizationEngine):
 	def __init__(self, mqtt_client):
 		super().__init__(mqtt_client)
 
-		self.drones = {'d01': {"id": "d01", "pos": {"x": 40, "y": 20, "z": 10}}}
-		self.cars = {'c01': {"id": "c01", "pos": {"x": 40, "y": 20, "z": 10}}}
-		self.hubs = {'h01': {'id': 'h01'}}
-		self.orders = { 'o01': {"id": "p01", "orderId": "o01", "carrier": None, "destination": { "type": "hub", "id": "h01" }
-} }
+		# Opening JSON file
+		with open('assets/showcase_0.json') as f:
+			data = json.load(f)
+
+		self.drones = data['entities']['drones']
+		self.cars = data['entities']['cars']
+		self.hubs = data['entities']['hubs']
+
 
 	def on_message_order_placed(self, client, userdata, msg):
 		super().on_message_order_placed(client, userdata, msg)
@@ -121,170 +124,6 @@ class OptimizationEngineShowcase0(OptimizationEngine):
 			assert len(self.drones) == 1, 'There has to be exactly one drone.'
 			
 			hub, car, drone = list(self.hubs.values())[0], list(self.cars.values())[0], list(self.drones.values())[0]
-			
-			transaction_1 = {
-				"id": str(uuid4()),
-				"from": {"type": "car", "id": car['id']},
-				"to": {"type": "drone", "id": drone['id']},
-				"parcel": parcel
-			}
-
-			transaction_2 = {
-				"id": str(uuid4()),
-				"from": {"type": "drone", "id": drone['id']},
-				"to": {"type": "hub", "id": hub['id']},
-				"parcel": parcel
-			}
-
-			position_1 = {"lat": 0.0, "long": 0.0, "alt": 0.0}
-			position_2 = {"lat": 1.0, "long": 0.0, "alt": 0.0}
-			position_3 = {"lat": 0.0, "long": 1.0, "alt": 0.0}
-			position_4 = {"lat": 1.0, "long": 1.0, "alt": 0.0}
-
-			hub_mission = {
-				"id": str(uuid4()),
-				"tasks": [
-					{
-						"type": "pickup",
-						"state": "TaskState.notStarted",
-						"transaction": transaction_2
-					}
-				]
-			}
-
-			car_mission = {
-				"id": str(uuid4()),
-				"tasks": [
-					{
-						"type": "dropoff",
-						"state": "TaskState.notStarted",
-						"transaction": transaction_1
-					}
-				]
-			}
-
-			drone_mission = {
-				"id": str(uuid4()),
-				"tasks": [
-					{
-						"type": "move",
-						"state": "TaskState.notStarted",
-						"destination": position_2,
-						"minimumDuration": 10
-					},
-					{
-						"type": "pickup",
-						"state": "TaskState.notStarted",
-						"transaction": transaction_1
-					},
-					{
-						"type": "move",
-						"state": "TaskState.notStarted",
-						"destination": position_3,
-						"minimumDuration": 10
-					},
-					{
-						"type": "dropoff",
-						"state": "TaskState.notStarted",
-						"transaction": transaction_2
-					},
-					{
-						"type": "move",
-						"state": "TaskState.notStarted",
-						"destination": position_4,
-						"minimumDuration": 10
-					}
-				]
-			}
-
-			self.publish(f'hub/{hub["id"]}/mission', hub_mission)
-			self.publish(f'car/{car["id"]}/mission', car_mission)
-			self.publish(f'drone/{drone["id"]}/mission', drone_mission)
-	
-		except BaseException as e:
-			logging.warn(f'Could not send missions ({repr(e)})!')
-			self.publish(f'opt/{self.client.id}/error', repr(e))
-
-#	def on_message_add_dummy_entities(self, client, userdata, msg):
-#		if not self.hubs:
-#			hub_id = uuid4()
-#			self.hubs = { hub_id: { 'id': hub_id } }
-#		if not self.drones:
-#			drone_id = uuid4()
-#			self.drones = { drone_id: { 'id': drone_id } }
-#		if not self.cars:
-#			car_id = uuid4()
-#			self.cars = { car_id: {'id': car_id } }
-#
-
-class OptimizationEngineRealWorld0(OptimizationEngine):
-
-	def send_missions(self, parcel):
-		print('Drones:')
-		print(self.drones)
-		print('Cars:')
-		print(self.cars)
-		print('Hubs:')
-		print(self.hubs)
-
-
-class OptimizationEngineTestWorld0(OptimizationEngine):
-
-	def __init__(self, mqtt_client):
-		super().__init__(mqtt_client)
-
-		# Opening JSON file
-		with open('assets/showcase_0.json') as f:
-			data = json.load(f)
-
-		self.drones = data['entities']['drones']
-		self.cars = data['entities']['cars']
-		self.hubs = data['entities']['hubs']
-
-
-	def on_message_order_placed(self, client, userdata, msg):
-		super().on_message_order_placed(client, userdata, msg)
-
-		self.publish(f'opt/{self.client.id}/acknowledged', '')
-
-	def on_message_parcel_placed(self, client, userdata, msg):
-		super().on_message_parcel_placed(client, userdata, msg)
-
-		#self.publish(f'opt/{self.client.id}/acknowledged', '')
-
-
-	def send_transaction(self, parcel):
-
-		transaction_0 = {
-            "id": "12345",
-            "orderId": "1922193319441955",
-            "carrier": { "type": "drone", "id": "d00" },
-            "destination": { "type": "hub", "id": "h00" }
-		}
-
-		transaction_1 = {
-			"id": "1234",
-			"orderId": "1922193319441955",
-			"carrier": {"type": "hub", "id": "h00"},
-			"destination": {"type": "hub", "id": "h00"}
-		}
-
-		transaction_2 = {
-            "id": "12341",
-            "orderId": "1922193319441955",
-            "carrier": { "type": "hub", "id": "h00" },
-            "destination": { "type": "hub", "id": "h00" }
-}
-
-		self.publish(f'parcel/12345/transfer', transaction_0)
-		self.publish(f'parcel/12345/transfer', transaction_1)
-		self.publish(f'parcel/12345/delivered', transaction_2)
-
-	def send_missions(self, parcel):
-		try:
-
-			hub, car, drone = list(self.hubs.values())[0], list(self.cars.values())[0], list(self.drones.values())[0]
-			hub01 = list(self.hubs.values())[1]
 
 			transaction_0 = {
 				"id": str(uuid4()),
@@ -300,16 +139,9 @@ class OptimizationEngineTestWorld0(OptimizationEngine):
 				"parcel": parcel
 			}
 
-			transaction_2 = {
-				"id": str(uuid4()),
-				"from": {"type": "hub", "id": hub01['id']},
-				"to": {"type": "car", "id": car['id']},
-				"parcel": parcel
-			}
-
-			position_0 = {"lat": -50.0, "long": 50.0, "alt": 0.0}
-			position_1 = {"lat": 0.0, "long": 50.0, "alt": 0.0}
-			position_2 = {"lat": 50.0, "long": 50.0, "alt": 0.0}
+			position_0 = {"lat": 49.36252, "long": 86.8661, "alt": 0}
+			position_1 = {"lat": 49.36219, "long": 86.8565, "alt": 0}
+			position_2 = {"lat": 49.36217, "long": 86.8606, "alt": 0}
 
 			hub01_mission = {
 				"id": str(uuid4()),
@@ -342,10 +174,10 @@ class OptimizationEngineTestWorld0(OptimizationEngine):
 						"transaction": transaction_2
 					},
 					{
-                        "type": "move",
-                        "state": "TaskState.notStarted",
-                        "destination": position_1,
-                        "minimumDuration": 1
+						"type": "move",
+						"state": "TaskState.notStarted",
+						"destination": position_1,
+						"minimumDuration": 1
 					},
 					{
 						"type": "dropoff",
@@ -403,5 +235,71 @@ class OptimizationEngineTestWorld0(OptimizationEngine):
 #			car_id = uuid4()
 #			self.cars = { car_id: {'id': car_id } }
 #
+
+#class OptimizationEngineRealWorld0(OptimizationEngine):
+#
+#	def send_missions(self, parcel):
+#		print('Drones:')
+#		print(self.drones)
+#		print('Cars:')
+#		print(self.cars)
+#		print('Hubs:')
+#		print(self.hubs)
+
+
+class OptimizationEngineTestWorld0(OptimizationEngine):
+
+	def __init__(self, mqtt_client):
+		super().__init__(mqtt_client)
+
+		# Opening JSON file
+		with open('assets/showcase_0.json') as f:
+			data = json.load(f)
+
+		self.drones = data['entities']['drones']
+		self.cars = data['entities']['cars']
+		self.hubs = data['entities']['hubs']
+
+
+	def on_message_order_placed(self, client, userdata, msg):
+		super().on_message_order_placed(client, userdata, msg)
+
+		self.publish(f'opt/{self.client.id}/acknowledged', '')
+
+	def on_message_parcel_placed(self, client, userdata, msg):
+		super().on_message_parcel_placed(client, userdata, msg)
+
+		#self.publish(f'opt/{self.client.id}/acknowledged', '')
+
+
+	def send_transaction(self, parcel):
+
+		transaction_0 = {
+            "id": "12345",
+            "orderId": "1922193319441955",
+            "carrier": { "type": "drone", "id": "d00" },
+            "destination": { "type": "hub", "id": "h00" }
+		}
+
+		transaction_1 = {
+			"id": "1234",
+			"orderId": "1922193319441955",
+			"carrier": {"type": "hub", "id": "h00"},
+			"destination": {"type": "hub", "id": "h00"}
+		}
+
+		transaction_2 = {
+            "id": "12341",
+            "orderId": "1922193319441955",
+            "carrier": { "type": "hub", "id": "h00" },
+            "destination": { "type": "hub", "id": "h00" }
+}
+
+		self.publish(f'parcel/12345/transfer', transaction_0)
+		self.publish(f'parcel/12345/transfer', transaction_1)
+		self.publish(f'parcel/12345/delivered', transaction_2)
+
+	def send_missions(self, parcel):
+		#never called
 
 
