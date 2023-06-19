8e9cca15ad3669c8b62d7145e4b0a19068125a60
johannespernpeintner <johannes.pernpeintner@tum.de>
2023-06-19 08:27:00 +0200

    Selected showcase0

--- opt/src/optimization_engine/optimization_engine.py
+++ opt/src/optimization_engine/optimization_engine.py
@@ -51,13 +51,13 @@ class OptimizationEngine:
 			project, version, entity, entity_id, _, parcel_id, *args = str(msg.topic).split('/')
 			logging.debug(f'Received parcel/placed message. Current orders: {self.orders}')
 			parcel = next(filter(lambda order: order['id'] == parcel_id, self.orders.values()))
-			parcel['carrier'] = { 'type': entity, 'id': entity_id }
+			parcel['carrier'] = {'type': entity, 'id': entity_id}
 
 			logging.debug(f'Parcel placed ({parcel})!')
 			self.publish(f'parcel/{parcel_id}/transfer', parcel)
 
-			self.send_transaction(parcel)
-			#self.send_missions(parcel)
+			self.send_missions(parcel)
+		#    self.send_missions(parcel)
 
 		except StopIteration as e:
 			logging.warn(f'Placed parcel not found in orders!')
@@ -90,6 +90,7 @@ class OptimizationEngineTest0(OptimizationEngine):
 			'+/+/status': self.on_message_status
 		}
 
+
 class OptimizationEngineShowcase0(OptimizationEngine):
 	def __init__(self, mqtt_client):
 		super().__init__(mqtt_client)
@@ -101,7 +102,6 @@ class OptimizationEngineShowcase0(OptimizationEngine):
 		self.drones = data['entities']['drones']
 		self.cars = data['entities']['cars']
 		self.hubs = data['entities']['hubs']
-
 
 	def on_message_order_placed(self, client, userdata, msg):
 		super().on_message_order_placed(client, userdata, msg)
@@ -119,7 +119,7 @@ class OptimizationEngineShowcase0(OptimizationEngine):
 			assert len(self.hubs) == 1, 'There has to be exactly one hub.'
 			assert len(self.cars) == 1, 'There has to be exactly one car.'
 			assert len(self.drones) == 1, 'There has to be exactly one drone.'
-			
+
 			hub, car, drone = list(self.hubs.values())[0], list(self.cars.values())[0], list(self.drones.values())[0]
 
 			transaction_0 = {
@@ -150,9 +150,9 @@ class OptimizationEngineShowcase0(OptimizationEngine):
 			position_1 = data['topology']['nodes']['n01']['position']
 			position_2 = data['topology']['nodes']['n02']['position']
 
-#           position_0 = {"lat": 49.36252, "long": 86.8661, "alt": 0}
-#           position_1 = {"lat": 49.36219, "long": 86.8565, "alt": 0}
-#           position_2 = {"lat": 49.36217, "long": 86.8606, "alt": 0}
+			#           position_0 = {"lat": 49.36252, "long": 86.8661, "alt": 0}
+			#           position_1 = {"lat": 49.36219, "long": 86.8565, "alt": 0}
+			#           position_2 = {"lat": 49.36217, "long": 86.8606, "alt": 0}
 
 			hub_mission = {
 				"id": str(uuid4()),
@@ -223,6 +223,7 @@ class OptimizationEngineShowcase0(OptimizationEngine):
 			logging.warn(f'Could not send missions ({repr(e)})!')
 			self.publish(f'opt/{self.client.id}/error', repr(e))
 
+
 #	def on_message_add_dummy_entities(self, client, userdata, msg):
 #		if not self.hubs:
 #			hub_id = uuid4()
@@ -259,7 +260,6 @@ class OptimizationEngineTestWorld0(OptimizationEngine):
 		self.cars = data['entities']['cars']
 		self.hubs = data['entities']['hubs']
 
-
 	def on_message_order_placed(self, client, userdata, msg):
 		super().on_message_order_placed(client, userdata, msg)
 
@@ -268,16 +268,14 @@ class OptimizationEngineTestWorld0(OptimizationEngine):
 	def on_message_parcel_placed(self, client, userdata, msg):
 		super().on_message_parcel_placed(client, userdata, msg)
 
-		#self.publish(f'opt/{self.client.id}/acknowledged', '')
-
+	# self.publish(f'opt/{self.client.id}/acknowledged', '')
 
 	def send_transaction(self, parcel):
-
 		transaction_0 = {
-            "id": "12345",
-            "orderId": "1922193319441955",
-            "carrier": { "type": "drone", "id": "d00" },
-            "destination": { "type": "hub", "id": "h00" }
+			"id": "12345",
+			"orderId": "1922193319441955",
+			"carrier": {"type": "drone", "id": "d00"},
+			"destination": {"type": "hub", "id": "h00"}
 		}
 
 		transaction_1 = {
@@ -288,10 +286,10 @@ class OptimizationEngineTestWorld0(OptimizationEngine):
 		}
 
 		transaction_2 = {
-            "id": "12341",
-            "orderId": "1922193319441955",
-            "carrier": { "type": "hub", "id": "h00" },
-            "destination": { "type": "hub", "id": "h00" }
+			"id": "12341",
+			"orderId": "1922193319441955",
+			"carrier": {"type": "hub", "id": "h00"},
+			"destination": {"type": "hub", "id": "h00"}
 		}
 
 		self.publish(f'parcel/12345/transfer', transaction_0)
