d3f6ee7a7531f10ddd5a73d1168d78131bc5344f
johannespernpeintner <johannes.pernpeintner@tum.de>
2023-06-20 06:59:22 +0200

    Update car.js

--- sim/src/models/car.js
+++ sim/src/models/car.js
@@ -156,7 +156,7 @@ class Car {
             simulator.publish(`${transaction.from.type}/${transaction.from.id}`, `transaction/${transaction.id}/ready`);
             this.state = CarState.waitingForTransaction;
             task.state = TaskState.waitingForTransaction;
-            simulator.publish(`${transaction.from.type}/${transaction.from.id}`, `car_placed`);
+            simulator.publish(`${transaction.from.type}/${transaction.from.id}`, "car_placed");
         } else {
                 this.state = CarState.waitingForTransaction;
                 task.state = TaskState.waitingForTransaction;
