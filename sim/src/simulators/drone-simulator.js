56323dc882669aa50b5d01658894067c8690927c
johannespernpeintner <johannes.pernpeintner@tum.de>
2023-06-22 07:25:46 +0200

    add debug message in drone-simulator.js

--- sim/src/simulators/drone-simulator.js
+++ sim/src/simulators/drone-simulator.js
@@ -84,8 +84,8 @@ module.exports = class DroneSimulator extends MQTTClient {
 
         try {
             if (this.matchTopic(topic, 'visualization/#')) {
-                console.log(`-- drone init ${topic.id`)
-                this.publish(`drone/${topic.id}`, 'init')
+                console.log(`-- drone init ${topic.id`);
+                this.publish(`drone/${topic.id}`, 'init');
                 if (['start', 'pause', 'resume', 'stop', 'reset'].includes(topic.rest)) {
                     this[topic.rest]();
                 }
